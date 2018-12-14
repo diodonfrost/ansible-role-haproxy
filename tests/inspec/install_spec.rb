6e29ad7f9d8be87c94233895c706b507d7de95af
diodonfrost <diodon.frost@diodonfrost.me>
2018-12-14 17:40:39 +0100

    add check service test in inspec

--- tests/inspec/install_spec.rb
+++ tests/inspec/install_spec.rb
@@ -1,6 +1,7 @@
 # Haproxy install
 
 haproxy_package = 'haproxy'
+haproxy_service = 'haproxy'
 
 control 'install-01' do
   impact 1.0
@@ -8,5 +9,15 @@ control 'install-01' do
   desc 'Haproxy should be installed'
   describe package(haproxy_package) do
     it { should be_installed }
+  end
+end
+
+control 'install-02' do
+  impact 1.0
+  title 'Haproxy service'
+  desc 'Haproxy should be enabled and started'
+  describe service(haproxy_service) do
+    it { should be_enabled }
+    it { should be_running }
   end
 end
