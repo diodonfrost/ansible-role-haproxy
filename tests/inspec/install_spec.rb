cb07304d032c175be203e624295cb28e807225d1
diodonfrost <diodon.frost@diodonfrost.me>
2018-12-18 11:19:53 +0100

    test haproxy user and group with inspec

--- tests/inspec/install_spec.rb
+++ tests/inspec/install_spec.rb
@@ -2,6 +2,7 @@
 
 haproxy_package = 'haproxy'
 haproxy_service = 'haproxy'
+haproxy_user = 'haproxy'
 
 control 'install-01' do
   impact 1.0
@@ -19,5 +20,16 @@ control 'install-02' do
   describe service(haproxy_service) do
     it { should be_enabled }
     it { should be_running }
+  end
+end
+
+control 'install-03' do
+  impact 1.0
+  title 'Haproxy user and group'
+  desc 'Haproxy user and group should be present'
+  describe user(haproxy_user) do
+    it { should exist }
+    its('group') { should eq 'haproxy' }
+    its('shell') { should eq '/sbin/nologin' }
   end
 end
